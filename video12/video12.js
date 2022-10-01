099febe4d83199befb4b201aedc51d412af680ad
vantri245 <vantri120997@gmail.com>
2022-10-01 21:04:16 +0700

    commit source video 12

---
+++ video12/video12.js
@@ -0,0 +1,15 @@
+console.log('Video 12')
+let arr = ['Một', 'Hai', 'Ba', 'Bốn', 'Năm']
+
+let i = 0;
+while (i < arr.length) {
+    console.log('Giá trị phần tử thứ ', i, 'là : ', arr[i]);
+    i++;
+}
+
+let j = arr.length;
+do {
+    j--;
+    console.log('Giá trị phần tử thứ ', j, 'là : ', arr[j]);
+
+} while (j != 0)
