40e8853d62ac555f0a7116c1b97d129518124acf
vantri245 <vantri120997@gmail.com>
2022-10-01 22:32:27 +0700

    commit source video18

---
+++ video18/video18.js
@@ -0,0 +1,11 @@
+console.log('Video 18')
+// function sum(a, b){
+//     return a + b;
+// }
+let a = 5;
+let b = 10;
+let sum = (a, b) => {
+    return a + b;
+}
+console.log('Tổng của ', a, 'và ', b, 'là: ', sum(a, b))
+
