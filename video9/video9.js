a9ce96206ea7f33e7280e092def0ea75f01d71d0
vantri245 <vantri120997@gmail.com>
2022-10-01 20:20:31 +0700

    commit source video9

---
+++ video9/video9.js
@@ -0,0 +1,9 @@
+console.log('Video 9')
+let arr = ['MU', 'CHEASE', 'LIVERPOOL']
+console.log('arr 1 :', arr)
+let arr2 = [
+    { name: 'Võ Văn Trí', age: '25' },
+    { name: 'Võ Thị Kim Yến', age: '25' },
+    { name: 'Nguyễn Dương Lễ', age: '28' }
+]
+console.log('arr 2 :', arr2)
