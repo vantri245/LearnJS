138de3524fd60a15d4cbf0210a16ce27004c110f
vantri245 <vantri120997@gmail.com>
2022-10-01 22:09:46 +0700

    commit source video14

---
+++ video14/video14.js
@@ -0,0 +1,26 @@
+console.log('Video 14')
+let day = '';
+switch (new Date().getDay()) {
+    case 0:
+        day = 'Sunday';
+        break;
+    case 1:
+        day = 'Monday';
+        break;
+    case 2:
+        day = 'Tuesday';
+        break;
+    case 3:
+        day = 'Wedesday';
+        break;
+    case 4:
+        day = 'Thursday';
+        break;
+    case 5:
+        day = 'Friday';
+        break;
+    case 6:
+        day = 'Saturday';
+        break;
+}
+console.log('Today is :', day);
